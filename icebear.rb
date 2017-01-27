0620e4971a6c220bbabb6e7f042bd0abe0ea5101
Luis <theluisrivera@gmail.com>
2017-01-26 21:33:23 -0800

    dry: single method to choose from possibilities

--- icebear.rb
+++ icebear.rb
@@ -20,10 +20,10 @@ def form_suffix(shape = nil, shape_two = nil)
   root_shape, root_shape_two, root = shape || rand(100), shape_two || rand(100), ""
   
   # first syllable
-  root << second_medial_consonant if root_shape < 95
-  root << second_vowel if !root_shape.between?(55,95)
-  root << second_final_consonant if root_shape_two.between?(10, 25) && !root_shape.between?(55,95)
-  root << second_medial_consonant if root_shape_two < 10 && !root_shape.between?(55,95)
+  root << random_phoneme(SECOND_MEDIAL_CONSONANT) if root_shape < 95
+  root << random_phoneme(SECOND_VOWEL) if !root_shape.between?(55,95)
+  root << random_phoneme(SECOND_FINAL_CONSONANT) if root_shape_two.between?(10, 25) && !root_shape.between?(55,95)
+  root << random_phoneme(SECOND_MEDIAL_CONSONANT) if root_shape_two < 10 && !root_shape.between?(55,95)
   root << second_syllable if root_shape_two < 10 && !root_shape.between?(55,95)
   root
 end
@@ -32,189 +32,120 @@ def form_root(shape = nil, shape_two = nil)
   root_shape, root_shape_two, root = shape || rand(100), shape_two || rand(100), ""
   
   # first syllable
-  root << first_consonant if root_shape < 75
-  root << first_vowel 
-  root << second_final_consonant if root_shape_two.between?(21, 88)
-  root << second_medial_consonant if root_shape_two < 21
+  root << random_phoneme(FIRST_CONSONANT) if root_shape < 75
+  root << random_phoneme(FIRST_VOWEL) 
+  root << random_phoneme(SECOND_FINAL_CONSONANT) if root_shape_two.between?(21, 88)
+  root << random_phoneme(SECOND_MEDIAL_CONSONANT) if root_shape_two < 21
   root << second_syllable if root_shape_two < 21
   root
 end
 
-def first_consonant
-  consonant = { 
-    6 => %w{t k s m n},
-    5 => %w{p b d g f l z ḿ ń},
-    4 => %w{j b́ d́ ǵ ĺ ź ḱ ḱ t̀ k̀ m̀ ǹ},
-    3 => %w{x j́ ʼ ṕ p̀ b̀ d̀ g̀ q},
-    2 => %w{m̂ n̂ q̀},
-    1 => %w{h b̂ d̂ ĝ q́}
-  }
-  
-  chosen = rand 41
-  case 
-  when chosen < 4 then consonant[1].random
-  when chosen < 9 then consonant[2].random
-  when chosen < 15 then consonant[3].random
-  when chosen < 22 then consonant[4].random
-  when chosen < 31 then consonant[5].random
-  else consonant[6].random
+def random_phoneme possibilities
+  case possibilities.size
+  when 6
+    chosen = rand 41
+    return case 
+    when chosen < 4 then possibilities[1].random
+    when chosen < 9 then possibilities[2].random
+    when chosen < 15 then possibilities[3].random
+    when chosen < 22 then possibilities[4].random
+    when chosen < 31 then possibilities[5].random
+    else possibilities[6].random
+    end
+  when 5
+    chosen = rand 39
+    return case 
+    when chosen < 5 then possibilities[1].random
+    when chosen < 12 then possibilities[2].random
+    when chosen < 20 then possibilities[3].random
+    when chosen < 29 then possibilities[4].random
+    else possibilities[5].random
+    end
   end
 end
 
-def first_vowel
-  vowel = { 
-    5 => %w{i},
-    4 => %w{u í},
-    3 => %w{a ú},
-    2 => %w{e á o é},
-    1 => %w{à ó ò â ô h},
-  }
+FIRST_CONSONANT = { 
+  6 => %w{t k s m n},
+  5 => %w{p b d g f l z ḿ ń},
+  4 => %w{j b́ d́ ǵ ĺ ź ḱ ḱ t̀ k̀ m̀ ǹ},
+  3 => %w{x j́ ʼ ṕ p̀ b̀ d̀ g̀ q},
+  2 => %w{m̂ n̂ q̀},
+  1 => %w{h b̂ d̂ ĝ q́}
+}
 
-  chosen = rand 39
-  case 
-  when chosen < 5 then vowel[1].random
-  when chosen < 12 then vowel[2].random
-  when chosen < 20 then vowel[3].random
-  when chosen < 29 then vowel[4].random
-  else vowel[5].random
-  end
-end
+FIRST_VOWEL = { 
+  5 => %w{i},
+  4 => %w{u í},
+  3 => %w{a ú},
+  2 => %w{e á o é},
+  1 => %w{à ó ò â ô h}
+}
 
-def second_medial_consonant
-  consonant = { 
-    6 => %w{t k s m n},
-    5 => %w{p b d g f l z ḿ ń},
-    4 => %w{j b́ d́ ǵ ĺ ź},
-    3 => %w{t́ ḱ t̀ k̀ m̀ ǹ x j́ ʼ q},
-    2 => %w{ṕ p̀ b̀ d̀ g̀ q̀},
-    1 => %w{m̂ n̂ h q́}
-    # none: BQ DQ GQ 
-  }
-  
-  chosen = rand 41
-  case 
-  when chosen < 4 then consonant[1].random
-  when chosen < 9 then consonant[2].random
-  when chosen < 15 then consonant[3].random
-  when chosen < 22 then consonant[4].random
-  when chosen < 31 then consonant[5].random
-  else consonant[6].random
-  end
-end
+SECOND_MEDIAL_CONSONANT = { 
+  6 => %w{t k s m n},
+  5 => %w{p b d g f l z ḿ ń},
+  4 => %w{j b́ d́ ǵ ĺ ź},
+  3 => %w{t́ ḱ t̀ k̀ m̀ ǹ x j́ ʼ q},
+  2 => %w{ṕ p̀ b̀ d̀ g̀ q̀},
+  1 => %w{m̂ n̂ h q́}
+  # none: BQ DQ GQ 
+}
 
-def second_final_consonant
-  consonant = { 
-    6 => %w{t k s m n},
-    5 => %w{p b d g f l z ḿ ń},
-    4 => %w{j b́ d́ ǵ ĺ ź t́ ḱ t̀ k̀ m̀ ǹ},
-    3 => %w{x j́ ʼ ṕ p̀ b̀ d̀ g̀ q},
-    2 => %w{m̂ n̂ q̀},
-    1 => %w{h b̂ d̂ ĝ q́}
-  }
-  
-  chosen = rand 41
-  case 
-  when chosen < 4 then consonant[1].random
-  when chosen < 9 then consonant[2].random
-  when chosen < 15 then consonant[3].random
-  when chosen < 22 then consonant[4].random
-  when chosen < 31 then consonant[5].random
-  else consonant[6].random
-  end
-end
+SECOND_FINAL_CONSONANT = { 
+  6 => %w{t k s m n},
+  5 => %w{p b d g f l z ḿ ń},
+  4 => %w{j b́ d́ ǵ ĺ ź t́ ḱ t̀ k̀ m̀ ǹ},
+  3 => %w{x j́ ʼ ṕ p̀ b̀ d̀ g̀ q},
+  2 => %w{m̂ n̂ q̀},
+  1 => %w{h b̂ d̂ ĝ q́}
+}
+
+SECOND_VOWEL = { 
+  5 => %w{i},
+  4 => %w{u},
+  3 => %w{í a},
+  2 => %w{ú e o},
+  1 => %w{á é à ò},
+  # Out: O Ə Ɔ X
+}
+
+THIRD_MEDIAL_CONSONANT = { 
+  6 => %w{t k s m n},
+  5 => %w{p b d g f l z ḿ ń},
+  4 => %w{j b́ d́ ǵ ĺ ź},
+  3 => %w{t́ ḱ t̀ k̀ m̀ ǹ x j́ ʼ q},
+  2 => %w{ṕ p̀ b̀ d̀ g̀ q̀},
+  1 => %w{m̂ n̂ h q́}
+  # none: BQ DQ GQ 
+}
+
+THIRD_FINAL_CONSONANT = { 
+  6 => %w{t k s m n},
+  5 => %w{p b d g f l z ḿ ń},
+  4 => %w{j b́ d́ ǵ ĺ ź t́ ḱ t̀ k̀ m̀ ǹ},
+  3 => %w{x j́ ʼ ṕ p̀ b̀ d̀ g̀ q},
+  2 => %w{m̂ n̂ q̀},
+  1 => %w{h b̂ d̂ ĝ q́}
+}
+
+THIRD_VOWEL = { 
+  5 => %w{i},
+  4 => %w{u},
+  3 => %w{í a},
+  2 => %w{ú e o},
+  1 => %w{á é à ò},
+  # Out: O Ə Ɔ X
+}
 
 def second_syllable
   syllable_shape, syllable = rand(100), ""
   
 #  syllable << ":" if syllable_shape < 29
-  syllable << second_vowel
-  syllable << third_final_consonant if syllable_shape < 40 && syllable_shape >= 5
-  syllable << third_medial_consonant if syllable_shape < 5
-  syllable << third_vowel if syllable_shape < 5
+  syllable << random_phoneme(SECOND_VOWEL)
+  syllable << random_phoneme(THIRD_FINAL_CONSONANT) if syllable_shape < 40 && syllable_shape >= 5
+  syllable << random_phoneme(THIRD_MEDIAL_CONSONANT) if syllable_shape < 5
+  syllable << random_phoneme(THIRD_VOWEL) if syllable_shape < 5
   syllable
-end
-
-def second_vowel
-  vowel = { 
-    5 => %w{i},
-    4 => %w{u},
-    3 => %w{í a},
-    2 => %w{ú e o},
-    1 => %w{á é à ò},
-    # Out: O Ə Ɔ X
-  }
-
-  chosen = rand 39
-  case 
-  when chosen < 5 then vowel[1].random
-  when chosen < 12 then vowel[2].random
-  when chosen < 20 then vowel[3].random
-  when chosen < 29 then vowel[4].random
-  else vowel[5].random
-  end
-end
-
-def third_medial_consonant
-  consonant = { 
-    6 => %w{t k s m n},
-    5 => %w{p b d g f l z ḿ ń},
-    4 => %w{j b́ d́ ǵ ĺ ź},
-    3 => %w{t́ ḱ t̀ k̀ m̀ ǹ x j́ ʼ q},
-    2 => %w{ṕ p̀ b̀ d̀ g̀ q̀},
-    1 => %w{m̂ n̂ h q́}
-    # none: BQ DQ GQ 
-  }
-  
-  chosen = rand 41
-  case 
-  when chosen < 4 then consonant[1].random
-  when chosen < 9 then consonant[2].random
-  when chosen < 15 then consonant[3].random
-  when chosen < 22 then consonant[4].random
-  when chosen < 31 then consonant[5].random
-  else consonant[6].random
-  end
-end
-
-def third_final_consonant
-  consonant = { 
-    6 => %w{t k s m n},
-    5 => %w{p b d g f l z ḿ ń},
-    4 => %w{j b́ d́ ǵ ĺ ź t́ ḱ t̀ k̀ m̀ ǹ},
-    3 => %w{x j́ ʼ ṕ p̀ b̀ d̀ g̀ q},
-    2 => %w{m̂ n̂ q̀},
-    1 => %w{h b̂ d̂ ĝ q́}
-  }
-  
-  chosen = rand 41
-  case 
-  when chosen < 4 then consonant[1].random
-  when chosen < 9 then consonant[2].random
-  when chosen < 15 then consonant[3].random
-  when chosen < 22 then consonant[4].random
-  when chosen < 31 then consonant[5].random
-  else consonant[6].random
-  end
-end
-
-def third_vowel
-  vowel = { 
-    5 => %w{i},
-    4 => %w{u},
-    3 => %w{í a},
-    2 => %w{ú e o},
-    1 => %w{á é à ò},
-    # Out: O Ə Ɔ X
-  }
-
-  chosen = rand 39
-  case 
-  when chosen < 5 then vowel[1].random
-  when chosen < 12 then vowel[2].random
-  when chosen < 20 then vowel[3].random
-  when chosen < 29 then vowel[4].random
-  else vowel[5].random
-  end
 end
 
 class Array
